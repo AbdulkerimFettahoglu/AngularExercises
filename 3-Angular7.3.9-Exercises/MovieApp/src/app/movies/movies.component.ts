fe3aded66aa476c372fc7f2df2f5150038b7d672
AbdulkerimFettahoglu <abdulkerim.fettahoglu@gmail.com>
2020-04-04 20:29:27 +0300

    "movies" componen was accidiently removed. It's back. Helloworld was successfully did for "navbar" component.

---
+++ 3-Angular7.3.9-Exercises/MovieApp/src/app/movies/movies.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { Movie } from '../movie';
+import { MovieService } from '../movie.service';
+
+@Component({
+    selector: "movies", //<movies></movies>
+    //selector: ".movies" //<div class=movies></div>
+    //selector: "#movies" //<div id=movies></div>
+    templateUrl: "movies.component.html"
+})
+export class MoviesComponent {
+    array: Movie[];
+    selectedMovie: Movie;
+
+    constructor(private movieService: MovieService) {}
+
+    ngOnInit(): void {
+        this.movieService.getMovies().subscribe(movies=>{
+            this.array = movies;
+        });
+    }
+
+    onSelected(movie:Movie) {
+        this.selectedMovie = movie;
+    }
+} 
