fb2df6024707dd9f8e45d4eadcdaa30ac5ce67a6
AbdulkerimFettahoglu <abdulkerim.fettahoglu@gmail.com>
2020-04-04 20:47:34 +0300

    "dashboard" component was added and worked successfully.

--- 3-Angular7.3.9-Exercises/MovieApp/src/app/app-routing.module.ts
+++ 3-Angular7.3.9-Exercises/MovieApp/src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes,RouterModule } from '@angular/router';
 import { MoviesComponent } from './movies/movies.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 export const routes: Routes = [
-  { path: "movies", component: MoviesComponent}
+  { path: '',  redirectTo:'/dashboard', pathMatch:'full'},
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'movies', component: MoviesComponent }
 ];
 
 @NgModule({
