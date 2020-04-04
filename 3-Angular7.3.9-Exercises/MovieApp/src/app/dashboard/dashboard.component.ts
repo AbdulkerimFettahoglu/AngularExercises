2ba6664ea00d5bfc433e321d1a9f9ddd837f4352
AbdulkerimFettahoglu <abdulkerim.fettahoglu@gmail.com>
2020-04-04 22:22:13 +0300

    "dashboard" component was added succesfully.

--- 3-Angular7.3.9-Exercises/MovieApp/src/app/dashboard/dashboard.component.ts
+++ 3-Angular7.3.9-Exercises/MovieApp/src/app/dashboard/dashboard.component.ts
@@ -1,14 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { MovieService } from '../movie.service';
+import { Movie } from '../movie';
+import { Observable } from 'rxjs';
 
 @Component({
-  selector: 'dashboard',
+  selector: 'app-dashboard',
   templateUrl: './dashboard.component.html'
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  movies: Movie[];
+  moviesLength: number;
+  constructor(private movieService: MovieService) { }
 
   ngOnInit() {
+    this.getMovies();
+  }
+
+  getMovies() {
+    return this.movieService.getMovies().subscribe(returnObject => {
+      this.movies = returnObject;
+      this.moviesLength = returnObject.length;
+    });
   }
 
 }
--- 3-Angular7.3.9-Exercises/MovieApp/src/app/movie.datasource.ts
+++ 3-Angular7.3.9-Exercises/MovieApp/src/app/movie.datasource.ts
@@ -1,9 +1,13 @@
 import {Movie} from "./movie";
 
 export const Movies: Movie[] = [
-    {id: 1, name:"movie 1"},
-    {id: 2, name:"movie 2"},
-    {id: 3, name:"movie 3"},
-    {id: 4, name:"movie 4"},
-    {id: 5, name:"movie 5"}
+    {id: 1, name:"movie 1", description: "description 1", imangeUrl: "1.jpg"},
+    {id: 2, name:"movie 2", description: "description 2", imangeUrl: "2.jpg"},
+    {id: 3, name:"movie 3", description: "description 3", imangeUrl: "3.jpg"},
+    {id: 4, name:"movie 4", description: "description 4", imangeUrl: "4.jpg"},
+    {id: 5, name:"movie 5", description: "description 5", imangeUrl: "5.jpg"},
+    {id: 6, name:"movie 6", description: "description 6", imangeUrl: "6.jpg"},
+    {id: 7, name:"movie 7", description: "description 7", imangeUrl: "7.jpg"},
+    {id: 8, name:"movie 8", description: "description 8", imangeUrl: "8.jpg"},
+    {id: 9, name:"movie 9", description: "description 9", imangeUrl: "9.jpg"}
 ];
